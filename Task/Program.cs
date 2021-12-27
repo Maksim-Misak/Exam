3cde61c6fd1fd9a6355c39609158e9fe7b19eb3b
Мисак Максим <maximmisak@gmail.com>
2021-12-27 21:00:37 +0300

    Method create random array

--- Task/Program.cs
+++ Task/Program.cs
@@ -1,1 +1,9 @@
-﻿
+﻿int[] CreateArray()
+{
+    int[] array = new int[10];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(-100, 100);
+    }
+    return array;
+}
